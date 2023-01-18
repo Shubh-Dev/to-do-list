0b01135535b47e4de33af7f9b36e3c7058ac38e9
Shubh <shubh.scb@gmail.com>
2023-01-18 20:44:24 +0530

    LINTERS ERR FIX

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 const todos = [
-    {
-        description: "wash car",
-        completed: false,
-        index: 1
-    },
-    {
-        description: "buy grocery",
-        completed: false,
-        index: 2
-    },
-    {
-        description: "wash cloths",
-        completed: false,
-        index: 3
-    }
+  {
+    description: 'wash car',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'buy grocery',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'wash cloths',
+    completed: false,
+    index: 3,
+  },
 ];
 
-
-
-todos.map((todo) => {
-    document.querySelector("root").append(todo)
-});
+todos.map((todo) => document.querySelector('root').append(todo));
