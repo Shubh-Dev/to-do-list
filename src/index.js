e3aeac0d508d382107483717df940c8d4730685a
Shubh <shubh.scb@gmail.com>
2023-01-18 20:41:12 +0530

    ARRAY OF OBJECTS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+const todos = [
+    {
+        description: "wash car",
+        completed: false,
+        index: 1
+    },
+    {
+        description: "buy grocery",
+        completed: false,
+        index: 2
+    },
+    {
+        description: "wash cloths",
+        completed: false,
+        index: 3
+    }
+];
 
+
+
+todos.map((todo) => {
+    document.querySelector("root").append(todo)
+});
